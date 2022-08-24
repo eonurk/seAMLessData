3f72e5acb44feae4debc17bc2fe7dd06430d7864
eonurk <onurkara3594@gmail.com>
2022-08-24 13:19:59 +0300

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Single cell obj for deconvolution.
+#' The files were integrated with Seurat using healthy BM data from Triana et al, 2022 and Van Galen et al. 2020.
+#'
+#' @docType data
+#' @family datasets
+#' @usage data(scRef)
+#'
+#' @examples
+#' data(scRef)
+"scRef"
---
+++ README.md
@@ -0,0 +1,3 @@
+## seAMLessData
+
+A data package for [seAMLess](https://github.com/eonurk/seAMLess) package on CRAN. Due to package size limitations policies of CRAN, the single cell atlases used in the package are stored here and lazy-loaded.
